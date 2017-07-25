270fb1ecaa3d387b1260e536bed5ce351e12b2fe
Shedlia.Freeman <shedliaf@basecampcodingacademy.org>
2017-07-25 13:52:36 -0500

    [new feature] Ties is now available

--- core.py
+++ core.py
@@ -5,20 +5,12 @@ def names(roster,player_1, player_2):
     Takes in both names and either sends you to the next function
     of adds the name of the player
     
-    >>> names({'Shedlia':{'Wins': 0, 'Losses': 0 }, 'Jo': {'Wins': 0, 'Losses': 0 }, 'Penny': {'Wins': 0, 'Losses': 0 }}, 'Shedlia', 'Jo')
-    >>> names({'Shedlia':{'Wins': 0, 'Losses': 0 }, 'Jo': {'Wins': 0, 'Losses': 0 }, 'Penny': {'Wins': 0, 'Losses': 0 }}, 'Shedlia', 'Billy')
-    {'Shedlia':{'Wins': 0, 'Losses': 0 }, 'Jo':{'Wins': 0, 'Losses': 0 }, 'Billy':{'Wins': 0, 'Losses': 0 }, 'Penny':{'Wins': 0, 'Losses': 0 }}
-    >>> names({'Shedlia':{'Wins': 0, 'Losses': 0 }, 'Jo': {'Wins': 0, 'Losses': 0 }, 'Penny': {'Wins': 0, 'Losses': 0 }}, 'Dil', 'Jo')
-    {'Shedlia':{'Wins': 0, 'Losses': 0 }, 'Jo':{'Wins': 0, 'Losses': 0 }, 'Dil':{'Wins': 0, 'Losses': 0}, 'Penny':{'Wins': 0, 'Losses': 0 }}    
-    >>> names({'Shedlia':{'Wins': 0, 'Losses': 0 }, 'Jo': {'Wins': 0, 'Losses': 0 }, 'Penny': {'Wins': 0, 'Losses': 0 }}, 'Duke', 'Dil')
-    {'Shedlia':{'Wins': 0, 'Losses': 0 }, 'Jo':{'Wins': 0, 'Losses': 0 }, 'Dil':{'Wins': 0, 'Losses': 0}, 'Penny':{'Wins': 0, 'Losses': 0 }, 'Duke': {'Wins': 0, 'Losses': 0}}
-    >>> names({'Shedlia':{'Wins': 0, 'Losses': 0 }, 'Jo': {'Wins': 0, 'Losses': 0 }, 'Penny': {'Wins': 0, 'Losses': 0 }}, 'Penny', 'Jo')
     '''
     
     if not roster.get(player_1, False):
-        roster[player_1] = {'Wins': 0, 'Losses': 0}
+        roster[player_1] = {'Wins': 0, 'Losses': 0, 'Ties': 0}
     if not roster.get(player_2, False):
-        roster[player_2] = {'Wins': 0, 'Losses': 0}
+        roster[player_2] = {'Wins': 0, 'Losses': 0, 'Ties': 0}
     return roster
 
 def record_game(player_1, p1_score, player_2, p2_score, roster):
@@ -26,8 +18,11 @@ def record_game(player_1, p1_score, player_2, p2_score, roster):
     if p1_score > p2_score:
         roster[player_1]['Wins'] += 1
         roster[player_2]['Losses'] += 1
-        print(roster)
-    else:
+    if p1_score < p2_score:
         roster[player_2]['Wins'] += 1
         roster[player_1]['Losses'] += 1
+
+    if p1_score == p2_score:
+        roster[player_1]['Ties'] += 1
+        roster[player_2]['Ties'] += 1
     return roster
