2cf331aa900b3a4ea101bb5b9f7c593459d621ca
Shedlia.Freeman <shedliaf@basecampcodingacademy.org>
2017-09-20 10:08:34 -0500

    [Shell] Finished

--- shell.py
+++ shell.py
@@ -45,29 +45,5 @@ def main():
             exit()
 
 
-def one_vs_one_main():
-    print('Welcome to Ping Pong Records')
-
-    player_1 = input('what is your name: ').title()
-
-    player_2 = input('Opponent what is your name:').title()
-
-    new_roster = names(roster, player_1, player_2)
-
-    dict_to_file(new_roster)
-
-    p1_score = int(input('What was your score:'))
-
-    p2_score = int(input('What was your opponent\'s score: '))
-
-    new_record = record_game(player_1, p1_score, player_2, p2_score, roster)
-
-    dict_to_file(new_record)
-
-
-def two_vs_two_main():
-    print('Works')
-
-
 if __name__ == '__main__':
     main()
