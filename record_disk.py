df2216a2627b8f490f10867936d8de9214d765a6
Shedlia.Freeman <shedliaf@basecampcodingacademy.org>
2017-09-25 10:44:19 -0500

    fixed a bug in the recording system

--- record_disk.py
+++ record_disk.py
@@ -6,9 +6,16 @@ def give_roster():
     for item in contestants:
         sublist = item.split(' | ')
         roster[sublist[0]] = {
-            'Wins': int(sublist[1]),
-            'Losses': int(sublist[2]),
-            'Ties': int(sublist[3])
+            'Wins':
+            int(sublist[1]),
+            'Losses':
+            int(sublist[2]),
+            'Ties':
+            int(sublist[3]),
+            'W/L':
+            '{:2f}'.format(
+                float(sublist[1]) /
+                (float(float(sublist[1]) + float(sublist[2]))) * 100)
         }
     return roster
 
@@ -16,8 +23,8 @@ def give_roster():
 def dict_to_file(new_roster):
     message = 'Name, Wins, Losses, Ties\n'
     for name, d in new_roster.items():
-        message += '{} | {} | {} | {}\n'.format(name, d['Wins'], d['Losses'],
-                                                d['Ties'])
+        message += '{} | {} | {} | {} | {} \n'.format(
+            name, d['Wins'], d['Losses'], d['Ties'], d['W/L'])
     with open("Roster.txt", "w") as files:
         files.write(message)
 
@@ -26,6 +33,6 @@ def read_roster():
     with open("Roster.txt", "r") as files:
         files.readline()
         contestants = files.readlines()
-    message = 'Name | Wins | Losses | Ties\n'
+    message = 'Name | Wins | Losses | Ties | Win/ Loss % \n'
 
     return (message + '\n'.join(contestants))
